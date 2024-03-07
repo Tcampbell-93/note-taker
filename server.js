8338ffe7931db71b080e0e7f68e60ddd437b1c82
Tcampbell93 <tcampbell_93@hotmail.com>
2024-03-07 10:56:46 -0800

    fixed bug with heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const api = require('./routes/index');
 
 // sets up port to be used if none choosen use 3001
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 // setting up middleware
 const app = express();
